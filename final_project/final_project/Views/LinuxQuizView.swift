a54b55c3ec4430ddf23003ad98f74f90168772b9
GhassenJamoussi99 <ghassen.jamoussi99@gmail.com>
2022-06-13 14:40:27 +0200

    fixate linux quiz page

--- final_project/final_project/Views/LinuxQuizView.swift
+++ final_project/final_project/Views/LinuxQuizView.swift
@@ -16,34 +16,75 @@ struct LinuxQuizView: View {
     var body: some View {
       GeometryReader{ geo in
         VStack {
-             HStack{
-                Text("Linux Quiz")
-                    .bold()
-                    .font(.system(size: 35))
-                    .foregroundColor(Color.white)
-             }.frame(maxWidth: .infinity, alignment: .center)
+            VStack{
+                 HStack{
+                    Text("Linux Quiz")
+                        .bold()
+                        .font(.system(size: 35))
+                        .foregroundColor(Color.white)
+                 }.padding(.bottom,10)
+                
+                
+                 HStack{
+                    Text("Test your Linux knowledge by answering these 10 interview questions!")
+                       .font(.system(size: 18))
+                       .foregroundColor(Color.white)
+                       .fixedSize(horizontal: false, vertical: true)
+                 }.padding(.bottom,10)
 
-     
-             HStack{
-                 Text("Test your Linux knowledge by answering these 10 interview questions!")
-                    .font(.system(size: 18))
-                    .foregroundColor(Color.white)
-             }
-            
-             //Buttons
-             HStack(alignment: .center)
+                 HStack(alignment: .center)
+                 {
+                   Image("linux_img")
+                       .resizable()
+                       .aspectRatio(contentMode: .fit)
+                       .frame(width: geo.size.width/2, height: geo.size.height/3)
+                 }
+
+             }.frame(maxWidth:.infinity, maxHeight: .infinity, alignment: .top)
+              .padding(20)
+        
+             //Question Details
+             HStack()
              {
                 showQuestionOptions()
-                
-             }.padding(10)
-              .frame(maxWidth: .infinity, alignment: .center)
+             }.frame(maxWidth:.infinity, alignment: .center)
               .background(Color.white)
               .clipShape(Rectangle())
               .cornerRadius(10)
-                
+              .padding(5)
+            
+            HStack(alignment: .center){
+                Button(action: {
+                   //ConcentrationGameVM.isTrueClicked()
+                }, label: {
+                Text("Next")
+                    .font(.system(size:30))
+                    .frame(width:geo.size.width/3, height:geo.size.height/10)
+                    .background(Color.gray)
+                    .foregroundColor(Color.white)
+                    .clipShape(Rectangle())
+                    .cornerRadius(10)
+                })
+                    
+
+                Button(action: {
+                   // ConcentrationGameVM.isFalseClicked()
+                }, label: {
+                Text("Previous")
+                    .font(.system(size:30))
+                    .frame(width:geo.size.width/3, height:geo.size.height/10)
+                    .background(Color.gray)
+                    .foregroundColor(Color.white)
+                    .clipShape(Rectangle())
+                    .cornerRadius(10)
+                })
+                //.frame(height: ConcentrationGameVM.isStartVisible ? 0 : nil)
+               // .disabled(ConcentrationGameVM.isStartVisible)
+
+            }.frame(maxHeight: .infinity, alignment: .bottom)
           }.frame(maxWidth: .infinity, maxHeight: .infinity, alignment:.center)
-            .background(.black)
-            .navigationBarTitle("Back")
+           .background(.black)
+           .navigationBarTitle("Back")
         }
     }
 
@@ -56,48 +97,109 @@ struct LinuxQuizView: View {
                     .font(.system(size:20))
                     .bold()
                     .foregroundColor(Color.black)
-            }
-            
+            }.frame(maxWidth: .infinity, alignment: .leading)
+             .padding(.bottom,5)
+          
             HStack{
-                if (!QuizVM.questions![QuizVM.getQuestionIndexInArray()].answers.answer_a!.isEmpty ) {
+                if (QuizVM.questions![QuizVM.getQuestionIndexInArray()].answers.answer_a != nil ) {
                     Image(systemName: QuizVM.tenRandomIDQuestionsStates[QuizVM.indexRandomQuestion]! ? "checkmark.square.fill" : "square")
                         .foregroundColor(QuizVM.tenRandomIDQuestionsStates[QuizVM.indexRandomQuestion]! ? Color(UIColor.systemBlue) : Color.secondary)
                             .onTapGesture {
                                 QuizVM.tenRandomIDQuestionsStates[QuizVM.indexRandomQuestion]!.toggle()
-                             }
+                            }
                     
                     Text(QuizVM.questions![QuizVM.getQuestionIndexInArray()].answers.answer_a!)
-                            .font(.system(size:20))
-                            .bold()
+                            .font(.system(size:15))
                             .foregroundColor(Color.black)
-                        }
-            /*
-              if (questions[idx].answers.answer_b != nil) {
+                }
+            }.frame(maxWidth: .infinity, alignment: .leading)
+              .padding(.bottom,5)
 
-              }
 
-              if (questions[idx].answers.answer_c != nil) {
+            HStack{
+                if (QuizVM.questions![QuizVM.getQuestionIndexInArray()].answers.answer_b != nil ) {
+                    Image(systemName: QuizVM.tenRandomIDQuestionsStates[QuizVM.indexRandomQuestion]! ? "checkmark.square.fill" : "square")
+                        .foregroundColor(QuizVM.tenRandomIDQuestionsStates[QuizVM.indexRandomQuestion]! ? Color(UIColor.systemBlue) : Color.secondary)
+                            .onTapGesture {
+                                QuizVM.tenRandomIDQuestionsStates[QuizVM.indexRandomQuestion]!.toggle()
+                                }
+                    
+                    Text(QuizVM.questions![QuizVM.getQuestionIndexInArray()].answers.answer_b!)
+                            .font(.system(size:15))
+                            .foregroundColor(Color.black)
+                }
+            }.frame(maxWidth: .infinity, alignment: .leading)
+              .padding(.bottom,5)
 
-              }
+            HStack{
+                if (QuizVM.questions![QuizVM.getQuestionIndexInArray()].answers.answer_c != nil ) {
+                    Image(systemName: QuizVM.tenRandomIDQuestionsStates[QuizVM.indexRandomQuestion]! ? "checkmark.square.fill" : "square")
+                        .foregroundColor(QuizVM.tenRandomIDQuestionsStates[QuizVM.indexRandomQuestion]! ? Color(UIColor.systemBlue) : Color.secondary)
+                            .onTapGesture {
+                                QuizVM.tenRandomIDQuestionsStates[QuizVM.indexRandomQuestion]!.toggle()
+                            }
+                    
+                    Text(QuizVM.questions![QuizVM.getQuestionIndexInArray()].answers.answer_c!)
+                            .font(.system(size:15))
+                            .foregroundColor(Color.black)
+                }
+            }.frame(maxWidth: .infinity, alignment: .leading)
+             .padding(.bottom,5)
 
-              if (questions[idx].answers.answer_d != nil) {
+                        
+            HStack{
+                if (QuizVM.questions![QuizVM.getQuestionIndexInArray()].answers.answer_d != nil ) {
+                    Image(systemName: QuizVM.tenRandomIDQuestionsStates[QuizVM.indexRandomQuestion]! ? "checkmark.square.fill" : "square")
+                        .foregroundColor(QuizVM.tenRandomIDQuestionsStates[QuizVM.indexRandomQuestion]! ? Color(UIColor.systemBlue) : Color.secondary)
+                            .onTapGesture {
+                                QuizVM.tenRandomIDQuestionsStates[QuizVM.indexRandomQuestion]!.toggle()
+                            }
+                    
+                    Text(QuizVM.questions![QuizVM.getQuestionIndexInArray()].answers.answer_d!)
+                            .font(.system(size:15))
+                            .foregroundColor(Color.black)
+                }
+            }.frame(maxWidth: .infinity, alignment: .leading)
+             .padding(.bottom,5)
 
-              }
 
-              if (questions[idx].answers.answer_e != nil) {
+            HStack{
+                if (QuizVM.questions![QuizVM.getQuestionIndexInArray()].answers.answer_e != nil ) {
+                    Image(systemName: QuizVM.tenRandomIDQuestionsStates[QuizVM.indexRandomQuestion]! ? "checkmark.square.fill" : "square")
+                        .foregroundColor(QuizVM.tenRandomIDQuestionsStates[QuizVM.indexRandomQuestion]! ? Color(UIColor.systemBlue) : Color.secondary)
+                            .onTapGesture {
+                                QuizVM.tenRandomIDQuestionsStates[QuizVM.indexRandomQuestion]!.toggle()
+                            }
+                    
+                    Text(QuizVM.questions![QuizVM.getQuestionIndexInArray()].answers.answer_e!)
+                            .font(.system(size:15))
+                            .foregroundColor(Color.black)
+                }
+            }.frame(maxWidth: .infinity, alignment: .leading)
+             .padding(.bottom,5)
 
-              }
+                        
+            HStack{
+                if (QuizVM.questions![QuizVM.getQuestionIndexInArray()].answers.answer_f != nil ) {
+                    Image(systemName: QuizVM.tenRandomIDQuestionsStates[QuizVM.indexRandomQuestion]! ? "checkmark.square.fill" : "square")
+                        .foregroundColor(QuizVM.tenRandomIDQuestionsStates[QuizVM.indexRandomQuestion]! ? Color(UIColor.systemBlue) : Color.secondary)
+                            .onTapGesture {
+                                QuizVM.tenRandomIDQuestionsStates[QuizVM.indexRandomQuestion]!.toggle()
+                            }
+                    
+                    Text(QuizVM.questions![QuizVM.getQuestionIndexInArray()].answers.answer_f!)
+                            .font(.system(size:15))
+                            .foregroundColor(Color.black)
+                }
+            }.frame(maxWidth: .infinity, alignment: .leading)
+             .padding(.bottom,5)
 
-              if (questions[idx].answers.answer_f != nil) {
-
-              }
-             */
-          }
-       }
+       }.padding(10)
     }
 }
 
 /*
+TODO: See how to make the viewBuilder simpler especially with the if statement (isEmpty)
                 ForEach(questions) { question in
                         Text("\(question.question) is a \(question.id)")
                     }
